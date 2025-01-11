88c95bd9b88302f0a753138f16fa4c60ad38559d
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-10 18:29:32 -0700

    got pg working

--- app.js
+++ app.js
@@ -1,67 +1,34 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const html = require('./html.js')
+const { handlePost } = require('./handlePost.js')
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(cors({ origin: 'https://jarenwhitehouse.netlify.app' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.get("/", (req, res) => res.type('html').send(html));
-app.post("/", (req, res) => {
-  const bodyJSON = req.body.JSON
-  res.json(bodyJSON)
-}
-);
+app.post("/dave", handlePost);
+
+
+app.post("/api", (req, res) => {
+  console.log(req.body?.made)
+  res.json({ 
+    message: `Hello from Render! This was made by ${req.body?.made}`,
+    method: "post"
+   })
+});
+
+app.post('/cred', (req,res) => {
+  console.log(process.env.PGHOST)
+  console.log(process.env.PGUSER)
+
+})
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-color: white;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: black;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
---
+++ handlePost.js
@@ -0,0 +1,18 @@
+const pg = require('pg')
+const { Client } = pg
+
+const handlePost = async (req, res) => {
+    const client = new Client();
+    await client.connect()
+    const jaren = await client.query('SELECT * FROM jarens')
+    const result = await client.query('SELECT NOW()')
+    await client.end()
+    console.log(req.body)
+    return res.json({ 
+        message: "test worked",
+        result: new Date(result["rows"][0]["now"]).toString().slice(0,15),
+        jaren: jaren["rows"][0],
+    })
+}
+
+module.exports = { handlePost }
