278dc8ad24af6d3b022289d28fab7f362899fc9f
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-17 15:48:23 -0700

    done with back for now, need to go get front ready

--- app.js
+++ app.js
@@ -6,32 +6,26 @@ const cors = require('cors');
 const port = process.env.PORT || 3001;
 // -----------------------------------
 const { handlePost, getJarens } = require('./handlePost.js')
-// const { poststring } = require('./postgresstring.js')
-const db = require('./models')
-db.sequelize.sync({ alter: true })
-.then((x)=>console.log(x))
-.catch((e)=>console.error(e))
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(`postgres://mysitedb_sgc8_user:${process.env.PGPASSWORD}@dpg-cu00qtogph6c73chd840-a.oregon-postgres.render.com:5432/mysitedb_sgc8`);
+
+// UNCOMMENT AND RUN LOCALLY TO SYNC AFTER ADDING MODELS
+// const db = require('./models')
+// db.sequelize.sync({ alter: true }).then((x)=>console.log(x)).catch((e)=>console.error(e))
+
 
 app.use(express.json());
 app.use(cors())
 app.use(cors({ origin: 'https://jarenwhitehouse.netlify.app' }));
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-const {Message, sequelize} = require('./models/message.js')
-
-// sequelize.sync({ alter: true }).then(
-//   console.log('Database synced!')    
-// ).catch((e)=>console.error(e))
-// Message.create({
-//       message: 'hey there',
-//       name: 'gary',
-//     }).then((data)=> {
-//       console.log('Message created:', data.toJSON())
-//     }).catch((e) => console.error(e))
-
-
+const { Message } = require('./models')
+app.post('/test', (req, res) => {
+    const {message, email, name } = req.body
+    Message.create({message, name, email})
+    .then((x)=> console.log(x.dataValues))
+    .then(console.log("added"))
+    .catch((e)=>console.error(e))
+    res.json({})
+})
 
 app.get('/', (req, res) => res.type('html').send(html))
 app.get("/api", getJarens)
